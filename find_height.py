a3758ce320eda4db16fa6710d6c84a8018858dab
Sen Lei <listen180@gmail.com>
2021-07-29 16:56:50 +0800

    major update

--- find_height.py
+++ find_height.py
@@ -95,6 +95,43 @@ def remove_above_and_fit(y_org, y_fit, y_line, loop_num=2):
     return y_temp
 
 
+
+
+
+def prune_data(y_org, curve, line):
+    """
+    """
+    y_modified = y_org.copy()
+    data_points_left = []
+    for i, xv in enumerate(x):
+        data_i = y_org[i]
+        y_curve_i = curve[i]
+        y_line_i = line[i]
+        if y_curve_i < y_line_i:
+            data_points_left.append(data_i)
+    data_avg = np.mean(data_points_left)
+    for i, xv in enumerate(x):
+        data_i = y_org[i]
+        y_curve_i = curve[i]
+        y_line_i = line[i]
+        if y_curve_i >= y_line_i:
+            y_modified[i] = data_avg
+    return y_modified
+
+
+def prune_and_fit(y_org, y_fit, y_line, loop_num=2):
+    """
+    """
+    for i in range(loop_num):
+        temp = prune_data(y_org, y_fit, y_line)
+        z_temp = np.polyfit(x, temp, 1, rcond=None, full=False, w=None, cov=False)
+        p_temp = np.poly1d(z_temp)
+        y_temp = p_temp(x)
+        y_line = y_temp
+    # print(p_temp)
+    return y_temp
+
+
 # =============================================================================
 # 
 # =============================================================================
@@ -106,6 +143,7 @@ def analysis(data_org, col_name):
     # Find Peak(s)
     # =============================================================================
     peaks, properties = find_peaks(y_org, width=10)
+    gutter, properties_gutter = find_peaks(-y_org, width=2)
     
     img_save_path = './imgs/plot_with_peak_'+col_name+'.png'
     plt.figure()
@@ -119,7 +157,18 @@ def analysis(data_org, col_name):
     # plt.show()
     plt.close()
     
+    x_arr = np.array(x)
+
     peaks_index = peaks
+    # peak_x = x[peaks_index[0]]
+    peak_x = x_arr[peaks].mean()
+    print(f"peak_x: {peak_x}")
+    
+    # gutter_x = x_arr[gutter]
+    # print(f"gutter_x: {gutter_x}")
+    # gutter_peak_right = gutter_x[gutter_x>peak_x][0]
+    # gutter_peak_left = gutter_x[gutter_x<peak_x][-1]
+    # print(f"gutter_peak_left: {gutter_peak_left}, gutter_peak_right: {gutter_peak_right}")
     
     # =============================================================================
     # Poly Fit
@@ -128,17 +177,73 @@ def analysis(data_org, col_name):
     z = np.polyfit(x, y_org, deg, rcond=None, full=False, w=None, cov=False)
     
     p1 = np.poly1d(z)
-    # print(p1)
+    # print(f"p1: {p1}")
     y_fit = p1(x)
     
     
     z2 = np.polyfit(x, y_org, 1, rcond=None, full=False, w=None, cov=False)
     p2 = np.poly1d(z2)
-    # print(p2)
+    # print(f"p2: {p2}")
     y_line = p2(x)
     
     
     y_support = remove_above_and_fit(y_org, y_fit, y_line, loop_num=2)
+    y_prune= prune_and_fit(y_org, y_fit, y_line, loop_num=1)
+
+
+    pp1 = np.polynomial.Polynomial.fit(x, y_org, deg)
+    pp2 = np.polynomial.Polynomial.fit(x, y_org, 1)
+    x_e = (pp1 - pp2).roots()
+    
+
+    x_e_gt_peak = x_e[(x_e > peak_x)]
+    x_e_lt_peak = x_e[(x_e < peak_x)]
+
+    # print("\n***"*3)
+    # print(f"x_e_gt_peak: {x_e_gt_peak}")
+    # print(f"x_e_lt_peak: {x_e_lt_peak}")
+    # print("\n***"*3)
+
+    x_left = x_e_lt_peak[-1]
+    x_right = x_e_gt_peak[0]
+
+    print(f"\nleft: {x_left}, peak: {peak_x}, right{x_right}")
+
+    ## Get polynomial changing points
+    # y = np.polyval(p1, x)
+    Q = np.polyder(p1)  # f'
+    xs = np.roots(Q)  # 求多项式函数根
+    xs = xs[(xs>x[0]) & (xs<x[-1])]
+
+    # print("\n********"*3)
+    # print(f"  Diff 2nd: {y_d2}")
+    # print(f"  xs: {xs}")
+
+
+    Q2 = np.polyder(Q)  # f''
+    y_d2 = np.polyval(Q2, xs)
+    is_gutter = [y_d2>0]
+    print(f"is_gutter: {is_gutter}")
+
+
+    # from itertools import compress
+    # xs_index = list(compress(range(len(is_gutter)), is_gutter))
+    xs = np.array(xs)
+    xs = xs[tuple(is_gutter)]
+
+
+    x_gutter_gt_peak = xs[(xs > peak_x)][0]
+    x_gutter_lt_peak = xs[(xs < peak_x)][-1]
+
+    xs = [x_gutter_lt_peak, x_gutter_gt_peak]
+    ys = np.polyval(p1, xs)
+
+    z_support = np.polyfit(xs, ys, 1, rcond=None, full=False, w=None, cov=False)
+    p_support = np.poly1d(z_support)
+    y_support_new = p_support(x)
+
+
+
     # =============================================================================
     # Plot
     # =============================================================================
@@ -148,6 +253,9 @@ def analysis(data_org, col_name):
     plt.plot(x, y_fit,'r',label='polyfit values')
     plt.plot(x, y_line,'yellow',label='fit line')
     plt.plot(x, y_support,'grey',label='support line')
+    # plt.plot(x, y_prune,'black',label='prune line')
+    plt.plot(x, y_support_new,'black',label='support line (new)')
+    plt.plot(xs, ys, "ro")
     plt.legend()
     
     plt.savefig(img_save_path, dpi=300)
